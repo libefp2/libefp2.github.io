baa3f22896d89d55a7e71666151716218c89e042
Jack Lawrence <jacklawrence441@gmail.com>
2024-12-03 10:16:33 -0500

    modified:   doc/applied_efp.rst 	modified:   examples/flex-EFP/Scripts/fragment_RMSD.py 	modified:   examples/flex-EFP/Scripts/make_AAs.py 	modified:   examples/flex-EFP/Scripts/make_bcls.py

--- examples/flex-EFP/Scripts/make_bcls.py
+++ examples/flex-EFP/Scripts/make_bcls.py
@@ -9,11 +9,16 @@ Head and tail groups are separate fragments.
 """
 
 import numpy as np
-import sys
 
 #site='359'
-g96_file=sys.argv[1]
-site=sys.argv[2]
+#g96_file=sys.argv[1]
+g96_file='efp_pair53004.g96'
+#site=sys.argv[2]
+
+tailside='C6'
+headside='C5'
+site=['752','754']
+oldres='752'
 
 def cut_frag(head,tail):
     desired_dist=1.07886
@@ -31,7 +36,7 @@ def cut_frag(head,tail):
 
 def make_inp(fragment):
     txt=[]
-    filename='t_'+fragment[0].split()[0]+'.inp'
+    filename='cla_t_'+fragment[0].split()[0]+'.inp'
     txt.append(' $contrl units=angs local=boys runtyp=makefp \n'+
 '       mult=1 icharg=0 coord=cart icut=11 $end\n'+
 ' $system timlim=99999   mwords=200 $end\n'+
@@ -49,7 +54,7 @@ def make_inp(fragment):
             if(atom.split()[2][0]=='M'):
                 col1=' '+atom.split()[2][0:2]+atom.split()[3]+' '
                 col2=at_sym['MG']
-                filename='h_'+atom.split()[0]+'.inp'
+                filename='cla_h_'+atom.split()[0]+'.inp'
             else:
                 col1=' '+atom.split()[2][0]+atom.split()[3]+'   '
                 col2=at_sym[atom.split()[2][0]]
@@ -77,12 +82,13 @@ at_sym={'H':'1.0','C':'6.0','N':'7.0','O':'8.0','MG':'12.0','P':'15.0','S':'16.0
         'NA':'11.0','CL':'17.0'
       }
 
-Rings=['MG','CHA','CHB','HB','CHC','HC','CHD','HD','NA','C1A','C2A','H2A','C3A','H3A','C4A',
-       'CMA','HMA1','HMA2','HMA3','NB','C1B','C2B','C3B','C4B','CMB','HMB1','HMB2','HMB3',
-       'CAB','OBB','CBB','HBB1','HBB2','HBB3','NC','C1C','C2C','H2C','C3C','H3C','C4C',
-       'CMC','HMC1','HMC2','HMC3','CAC','HAC1','HAC2','CBC','HBC1','HBC2','HBC3','ND',
-       'C1D','C2D','C3D','C4D','CMD','HMD1','HMD2','HMD3','CAD','OBD','CBD','HBD','CGD',
-       'O1D','O2D','CED','HED1','HED2','HED3']
+Rings=['MG','CHA','CHB','CHC','CHD','NA','C1A','C2A','C3A','C4A','CMA','NB','C1B','C2B','C3B',
+       'C4B','CMB','CAB','CBB','NC','C1C','C2C','C3C','C4C','CMC','CAC','CBC','ND','C1D','C2D',
+       'C3D','C4D','CMD','CAD','OBD','CBD','CGD','O1D','O2D','CED','CAA','CBA','CGA','O1A',
+       'O2A','C1','C2','C3','C4','C5','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11',
+       'H12','H13','H14','H15','H16','H17','H18','H19','H20','H21','H22','H23','H24','H25',
+       'H26','H27','H28','H29','H30','H31','H32','H33','H34','H35','H36','H37','H38','H39',
+       'H40','H41']
 
 #with open('bchl359-50028.g96','r') as g96:
 #    g0=g96.readlines()
@@ -90,19 +96,19 @@ Rings=['MG','CHA','CHB','HB','CHC','HC','CHD','HD','NA','C1A','C2A','H2A','C3A',
 with open(g96_file,'r') as g96:
     g0=g96.readlines()
     
-tailside='CAA'
-headside='C2A'
+#tailside and headside represent the atoms that are bound but are to be broken to create 
+#separate fragment files.
 curr_head=[]
 curr_tail=[]
 #BCLs=[]
-oldres='359'
 for line in g0:
-    if 'BCL' in line:
+    if 'CLA' in line:
         if(oldres!=line.split()[0]):
             head_coord,tail_coord = cut_frag(head_cut,tail_cut)
             curr_head.append(' H000    1.0      '+str("{:.8f}".format(head_coord[0]))+'       '+str("{:.8f}".format(head_coord[1]))+'       '+str("{:.8f}".format(head_coord[2]))+'\n')
             curr_tail.append(' H000    1.0      '+str("{:.8f}".format(tail_coord[0]))+'       '+str("{:.8f}".format(tail_coord[1]))+'       '+str("{:.8f}".format(tail_coord[2]))+'\n')
-            if(oldres!=site):
+            #if(oldres!=site):
+            if oldres not in site:
                 make_inp(curr_head)
                 make_inp(curr_tail)
             curr_head=[]
