f71f72bb6b83a1d0e374be082e557e4de66638b6
Jack Lawrence <jacklawrence441@gmail.com>
2025-02-06 13:51:42 -0500

    Commented and gave all scripts a fresh coat of paint.

--- examples/flex-EFP/Scripts/fragment_RMSD.py
+++ examples/flex-EFP/Scripts/fragment_RMSD.py
@@ -4,132 +4,252 @@ Created on Tue Aug 14 2024
 
 @author: jackl
 """
+
 import sys
 import numpy as np
 import os
 
-directory = '/depot/lslipche/data/yb_boss/flexible_efp/efpdb/'
-ang_cutoff=0.20                     # Minimum RMSD allowed for "good" match
-cutoff=ang_cutoff*1.8897259886      # Cutoff convert to Bohr
 
-inp=sys.argv[1]    
-#inp='g_952.inp'                 # a_22_304.inp for example. Input file with fragment coords
-with open(inp, "r") as orig:
-    orgs = orig.readlines()
-    
-outname=inp.replace('.inp','.efp')  # Output file name, same as input but changed extension.
-                                    # If no match is found, then outname will not be used.
+
+
+# Directory containing the library files. CHANGE#######################################
+base_directory = '/depot/lslipche/data/yb_boss/flexible_efp/efpdb/'
+
+ang_cutoff = 0.20                        # Minimum RMSD allowed for a "good" match (in Angstroms)
+bohr_cutoff = ang_cutoff * 1.8897259886  # Convert Angstrom cutoff to Bohr
+
+#Add to these if there are more 
+amino_acid_dict = {
+    'a': 'ala', 'r': 'arg', 'n': 'asn', 'd': 'asp', 'c': 'cys',
+    'q': 'gln', 'e': 'glu', 'g': 'gly', 'h': 'hip', 'i': 'ile',
+    'l': 'leu', 'k': 'lys', 'm': 'met', 'f': 'phe', 'p': 'pro',
+    's': 'ser', 't': 'thr', 'w': 'trp', 'y': 'tyr', 'v': 'val',
+    'hp': 'hip', 'hd': 'hid', 'he': 'hie'
+}
+
+# "M" below is Mg. If atoms are present in your system but not defined here
+#     you will need to add them here
+atom_weights = {
+    'H': 1.0, 'O': 15.9949100, 'C': 12.0000000,
+    'N': 12.0030700, 'S': 32.0650000, 'M': 23.3040000
+}
+
+
+# Get input filename from command-line argument
+inp = sys.argv[1]    
+#              For testing, you might use a fixed input file:
+# inp = 'g_952.inp'  # e.g., a_22_304.inp. Input file with fragment coordinates
+
+# Read input file
+with open(inp, "r") as orig_file:
+    input_lines = orig_file.readlines()
+
+# If good case found, output file name will be the same but .efp instead of .inp
+# (Note: This file is only used if a match is found.)
+outname = inp.replace('.inp', '.efp')
+
+
+
 
 def get_RMSD(coords1,coords2,mass):
+    """
+    Compute the weighted root-mean-square deviation (RMSD) between two sets of coordinates.
+    
+    Parameters:
+        coords1, coords2: 2D lists or arrays containing coordinate data.
+        weights: List of weights for each coordinate point.
+    
+    Returns:
+        Weighted RMSD.
+    """
     tot=0.0
     length=len(coords1)
     dim=len(coords1[0])
     totmass=0.0
+    
     for i in range(length):
-        for j in range(dim):
-            tot+=mass[i]*(coords1[i][j]-coords2[i][j])**2
+        #Sum distances for each atom, weighted by atomic mass
+        tot+=mass[i]*sum([(coords1[i][j]-coords2[i][j])**2.0 for j in range(dim)])
         totmass+=mass[i]
+        
+    # Normalized RMSD calculation.
     rmsd=np.sqrt(tot)/np.sqrt(totmass)
     return rmsd
 
 def kabsch_algorithm(coords, coords2):
-        
+    """
+    Compute the optimal rotation matrix using the Kabsch algorithm to align coords onto coords2
+    
+    Parameters:
+        coords: Coordinates to be rotated 
+        coords2: Reference coordinates 
+    
+    Returns:
+        rotation_matrix: Optimal rotation matrix.
+        com1: Center-of-mass (mean) of the first coordinates
+        com2: Center-of-mass (mean) of the second coordinates
+    """
+    # Compute centers of mass
     com1 = np.mean(coords, axis=0)
     com2 = np.mean(coords2, axis=0)
     
-    coords1_aligned = coords - com1
-    coords2_aligned=coords2 - com2
+    # Center the coordinates
+    coords1_centered = coords - com1
+    coords2_centered = coords2 - com2
     
-    covariance_matrix = np.dot(coords1_aligned.T, coords2_aligned)
-    #print(covariance_matrix)
+    # Compute the covariance matrix
+    covariance_matrix = np.dot(coords1_centered.T, coords2_centered)
     
+    # Compute the SVD of the covariance matrix
     u, _, vh = np.linalg.svd(covariance_matrix)
     rotation_matrix = np.dot(u, vh)
-
+    
+    #Check for unwanted reflections/make sure rotation is "right-handed"
     if np.linalg.det(rotation_matrix) < 0:
         rotation_matrix[:, -1] *= -1
+        
     return rotation_matrix, com1, com2
 
 def apply_transform(coords, rotation_matrix, com1, com2):
+    """
+    Reads:
+        coords: Coordinates to transform
+        rotation_matrix: Rotation matrix computed from the Kabsch algorithm
+        com1: Center-of-mass of the original coordinates
+        com2: Center-of-mass of the target coordinates
+    
+    Returns:
+        Transformed coordinates
+    """
+    # Translate coordinates to the origin
     translated_coords = coords - com1
-
+    # Rotate the translated coordinates.
     rotated_coords = np.dot(translated_coords, rotation_matrix)
-    rotated_coords+=com2
-    return rotated_coords
+    # Translate to the target center of mass
+    transformed_coords = rotated_coords + com2
+    return transformed_coords
 
-amino_acid_dict = {'a':'ala','r':'arg','n':'asn','d':'asp','c':'cys',
-                   'q':'gln','e':'glu','g':'gly','h':'hip','i':'ile',
-                   'l':'leu','k':'lys','m':'met','f':'phe','p':'pro',
-                   's':'ser','t':'thr','w':'trp','y':'tyr','v':'val',
-                   'hp':'hip','hd':'hid','he':'hie'}
-atom_weights = {'H':1.0,'O':15.9949100,'C':12.0000000,'N':12.0030700,'S':32.0650000,'M':23.3040000}
 
+amino_acid_dict = {
+    'a': 'ala', 'r': 'arg', 'n': 'asn', 'd': 'asp', 'c': 'cys',
+    'q': 'gln', 'e': 'glu', 'g': 'gly', 'h': 'hip', 'i': 'ile',
+    'l': 'leu', 'k': 'lys', 'm': 'met', 'f': 'phe', 'p': 'pro',
+    's': 'ser', 't': 'thr', 'w': 'trp', 'y': 'tyr', 'v': 'val',
+    'hp': 'hip', 'hd': 'hid', 'he': 'hie'
+}
+
+atom_weights = {
+    'H': 1.0, 'O': 15.9949100, 'C': 12.0000000,
+    'N': 12.0030700, 'S': 32.0650000, 'M': 23.3040000
+}
+
+
+
+
+# Extract the residue code from the input filename (assumes filename starts with a residue code)
 try:
-    res=amino_acid_dict[inp.split('_')[0]]
-except:
-    print('No library folder for: '+inp)
-    exit()
+    res = amino_acid_dict[inp.split('_')[0]]
+except KeyError:
+    print('No library folder for: ' + inp)
+    sys.exit()
 
-directory+=res
+directory = os.path.join(base_directory, res)
 
-file_extension = '.efp'  # change this to your desired file extension
+# Define the file extension for matching EFP files.
+file_extension = '.efp'
 
+# --------------------------
+# Read coordinates from the input file
+# --------------------------
 
-#Grab coordinates from the input file, convert from Angstroms to bohrs.
-start=0
-xyz=[]
-orig_coords=[]
-weights=[]
-inp_at_lines=[]
-for line in orgs:
+# (1 Angstrom = 0.529177249 Bohr)
+ANGSTROM_TO_BOHR = 0.529177249
+
+orig_coords = []  # list of [x, y, z] coordinates in Bohr
+weights = []      # list of atomic weights corresponding to each coordinate
+
+reading_coords = False
+for line in input_lines:
     if '$end' in line:
-        start=0
-    elif(start==1):
-        inp_at_lines.append(line)
-        xyz.append(float(line.split()[2])/0.529177249)
-        xyz.append(float(line.split()[3])/0.529177249)
-        xyz.append(float(line.split()[4])/0.529177249)
-        orig_coords.append(xyz)
-        xyz=[]
-        weights.append(atom_weights[line.split()[0][0]])
+        reading_coords = False
+    elif reading_coords:
+        parts = line.split()
+        # Convert coordinates from Angstroms to Bohr
+        x = float(parts[2]) / ANGSTROM_TO_BOHR
+        y = float(parts[3]) / ANGSTROM_TO_BOHR
+        z = float(parts[4]) / ANGSTROM_TO_BOHR
+        
+        orig_coords.append([x, y, z])
+        # Use the first character of the atom type to lookup the weight
+        try:
+            weights.append(atom_weights[parts[0][0]])
+        except:
+            print('Atom '+parts[0]+' is not defined. Define it and its atomic mass in `atom_weights`.')
     elif 'C1' in line:
-        start=1
-minrmsd=20.0     # Arbitrary starting value. This will be replaced with real RMSD values in the loop.
+        # Start reading coordinates after encountering 'C1'
+        reading_coords = True
 
-# Loop through files in the directory
+# --------------------------
+# Loop through EFP fragment files in the specified directory
+# --------------------------
+
+min_rmsd = 20.0  # Initialize with a large value; will be updated if a better match is found.
+match = None     # Will store the file path of the best match
+
+# Loop over files with the specified extension in the directory.
 for filename in os.listdir(directory):
-    if filename.endswith(file_extension):
-        full_path = os.path.join(directory, filename)
-        with open(full_path, "r") as term:
-            terms = term.readlines()
-    else:
-        continue
-    start=0
-    xyz=[]
-    term_coords=[]
-    for line in terms:
+    if not filename.endswith(file_extension):
+        continue  # Skip files that do not match the extension
+
+    full_path = os.path.join(directory, filename)
+
+    # Read the candidate file.
+    with open(full_path, "r") as candidate_file:
+        candidate_lines = candidate_file.readlines()
+
+    # Grab coordinates from the fragment file
+    # The coordinates are assumed to be in the section following the header "COORDINATES (BOHR)" 
+    # and ending when a line containing 'BO21' is encountered, this is the standard name for the first "bond midpoint"
+    candidate_coords = []
+    reading_candidate_coords = False
+    for line in candidate_lines:
         if 'BO21' in line:
-            start=0
-        elif(start==1):
-            xyz.append(float(line.split()[1]))
-            xyz.append(float(line.split()[2]))
-            xyz.append(float(line.split()[3]))
-            term_coords.append(xyz)
-            xyz=[]
+            reading_candidate_coords = False
+        elif reading_candidate_coords:
+            parts = line.split()
+            x = float(parts[1])
+            y = float(parts[2])
+            z = float(parts[3])
+            candidate_coords.append([x, y, z])
         elif 'COORDINATES (BOHR)' in line:
-            start=1
-    new_coords=np.array(term_coords)
-    coords2=np.array(orig_coords)
-    rotation_matrix, com1, com2 = kabsch_algorithm(new_coords, coords2)
-    aligned_new_coords = apply_transform(new_coords, rotation_matrix, com1,com2)
-    rmsd=(get_RMSD(aligned_new_coords,coords2,weights))
+            reading_candidate_coords = True
 
-    if(rmsd<minrmsd):
-        minrmsd=rmsd 
-        match=full_path
+    # Convert lists to numpy arrays
+    candidate_coords = np.array(candidate_coords)
+    fragment_coords = np.array(orig_coords)
 
-if(minrmsd<cutoff):
-    os.system("python step4.Flexible_V5.py "+full_path+' '+inp+' -d -xr')
+    # Use the Kabsch algorithm to compute the optimal rotation to impost structure 1 onto structure 2
+    rotation_matrix, com_candidate, com_fragment = kabsch_algorithm(candidate_coords, fragment_coords)
+
+    # Apply the transformation
+    aligned_candidate_coords = apply_transform(candidate_coords, rotation_matrix, com_candidate, com_fragment)
+
+    # Compute the weighted RMSD between the transformed coordinates and the input
+    current_rmsd = get_RMSD(aligned_candidate_coords, fragment_coords, weights)
+
+    # Update best match if current RMSD is lower.
+    if current_rmsd < min_rmsd:
+        min_rmsd = current_rmsd
+        match = full_path
+
+# --------------------------
+# Transform library .efp file to input coordinates if good match found.
+# --------------------------
+if min_rmsd < bohr_cutoff:
+    # If a good match is found, run the next script with the matching file and the input file.
+    os.system("python step4.Flexible_V5.py " + match + " " + inp + " -d -xr")
 else:
-    print('no match, run GAMESS for: '+inp)
-    #os.system('gms_slurm -p 20 -q standby -v 2023 '+inp)
+    # If no match is found, print a message (or launch GAMESS).
+    print('No match, run GAMESS for: ' + inp)
+    # Uncomment the following line if you want to run GAMESS:
+    # os.system('gms_slurm -p 20 -q standby -v 2023 ' + inp)
--- examples/flex-EFP/Scripts/make_bcls.py
+++ examples/flex-EFP/Scripts/make_bcls.py
@@ -4,125 +4,245 @@ Created on Tue Jul 16 14:42:41 2024
 
 @author: jackl
 
-reads in .g96 of efp region and creates .inp files for bacteriochlorophyl a molecules.
-Head and tail groups are separate fragments.
+This script reads in a .g96 file of the EFP region and a full configuration .g96 file,
+and then creates .inp files for bacteriochlorophyll a molecules. In this process, the
+head and tail groups are treated as separate fragments. 
+
+This is a "hard-coded" script, that is, this script is very specific to chlorophylls 
+or bacteriochlorophylls (specifically chloropyll a and bacteriochlorophyll a). 
+If you intend to separate a molecule into multiplefragment .efp files, you will 
+need to edit this script.
 """
 
 import numpy as np
 import sys
 
-#site='359'
-g96_file=sys.argv[1]
-#g96_file='efp_pair53004.g96'
-#site=sys.argv[2]
+# ---------------------------
+# Input files and cutting definitions
+# ---------------------------
+# Command-line arguments:
+# Sample execution: python make_bcls_V2.py efp_opt_83855.g96 optimized_83855.g96
+g96_file = sys.argv[1]        # EFP region file (e.g., "efp_opt_83855.g96")
+full_g96_file = sys.argv[2]   # Full configuration file (e.g., "optimized_83855.g96")
 
-tailside='C6'
-headside='C5'
-site=['752','754']
-oldres='752'
+# Settings for fragment splitting. the bond between atoms C5 and C6 is where the fragments split.
+tailside = 'C6'             # Atom name defining the tail side boundary
+headside = 'C5'             # Atom name defining the head side boundary
 
-def cut_frag(head,tail):
-    desired_dist=1.07886
-    xh=float(head.split()[4])*10
-    yh=float(head.split()[5])*10
-    zh=float(head.split()[6])*10
-    xt=float(tail.split()[4])*10
-    yt=float(tail.split()[5])*10
-    zt=float(tail.split()[6])*10
-    dist_mag=np.sqrt(((xh-xt)**2)+((yh-yt)**2)+((zh-zt)**2))
-    #print(dist_mag,desired_dist)
-    t_h=[((xh-xt)*desired_dist/dist_mag)+xt,((yh-yt)*desired_dist/dist_mag)+yt,((zh-zt)*desired_dist/dist_mag)+zt]
-    h_t=[((xt-xh)*desired_dist/dist_mag)+xh,((yt-yh)*desired_dist/dist_mag)+yh,((zt-zh)*desired_dist/dist_mag)+zh]
-    return h_t,t_h
+# List of QM residue IDs. If you want all CLA/BCL residues to be made into fragments, make this empty
+site = ['752', '754', '753', '790', '792', '793']  
+oldres = '751'              # Initial residue number for tracking changes
+
+# ---------------------------
+# Function Definitions
+# ---------------------------
+def cut_frag(head, tail):
+    """    
+    This function uses the coordinates from two atoms (head and tail) to compute
+    the positions for adding virtual hydrogens to cap the now-separated fragments
+    (along the vector that is the C5-C6 bond).
+    
+    Parameters:
+        head: A string line with the head atom lines.
+        tail: A string line with the tail atom lines.
+    
+    Returns:
+        h_t: coordinates [x, y, z] for the virtual hydrogen on the head side.
+        t_h: coordinates [x, y, z] for the virtual hydrogen on the tail side.
+    """
+    desired_dist = 1.07886  # Desired bond distance
+    # Scale coordinates by 10 (nm -> angstrom)
+    xh, yh, zh = [float(head.split()[i]) * 10 for i in range(4, 7)]
+    xt, yt, zt = [float(tail.split()[i]) * 10 for i in range(4, 7)]
+    # Calculate the magnitude of the distance vector between head and tail.
+    dist_mag = np.sqrt((xh - xt)**2 + (yh - yt)**2 + (zh - zt)**2)
+    # Compute the new coordinates along the head-to-tail vector.
+    h_t = [
+        ((xt - xh) * desired_dist / dist_mag) + xh,
+        ((yt - yh) * desired_dist / dist_mag) + yh,
+        ((zt - zh) * desired_dist / dist_mag) + zh
+    ]
+    # Compute the new coordinates along the tail-to-head vector.
+    t_h = [
+        ((xh - xt) * desired_dist / dist_mag) + xt,
+        ((yh - yt) * desired_dist / dist_mag) + yt,
+        ((zh - zt) * desired_dist / dist_mag) + zt
+    ]
+    return h_t, t_h
 
 def make_inp(fragment):
-    txt=[]
-    filename='cla_t_'+fragment[0].split()[0]+'.inp'
-    txt.append(' $contrl units=angs local=boys runtyp=makefp \n'+
-'       mult=1 icharg=0 coord=cart icut=11 $end\n'+
-' $system timlim=99999   mwords=200 $end\n'+
-' $scf soscf=.f. dirscf=.t. diis=.t. CONV=1.0d-06  $end\n'+
-' $basis gbasis=n31 ngauss=6 ndfunc=1 $end\n'+
-' $DAMP IFTTYP(1)=2,0 IFTFIX(1)=1,1 thrsh=500.0 $end\n'+
-' $MAKEFP  POL=.t. DISP=.f. CHTR=.f.  EXREP=.f. $end\n'+
-' $data\n'+
-' '+filename.split('.')[0]+'\n'+
-' C1\n')
+    """
+    Generate an input (.inp) file for a fragment of bacteriochlorophyll a.
+    The filename is based on the fragment's residue number and atom ID.
+    The file includes various control sections and a coordinate list.
+    
+    Parameters:
+        fragment: Lines of atoms comprising the fragment.
+    """
+    txt = []
+    # Extract residue number and atom ID from the first line of the fragment.
+    resnum = fragment[0].split()[0]
+    atomid = fragment[0].split()[3]
+    
+    # Choose a filename prefix based on whether the first fragment line contains 'MG'
+    if 'MG' in fragment[0]:
+        namestart = 'clah_'
+    else:
+        namestart = 'clat_'
+    filename = namestart + resnum + '_' + atomid + '.inp'
+    
+    # Construct the header for the input file.
+    # You may want to customize these paramaters.
+    header = (
+        " $contrl units=angs local=boys runtyp=makefp \n"
+        "       mult=1 icharg=0 coord=cart icut=11 $end\n"
+        " $system timlim=99999   mwords=4000 $end\n"
+        " $scf soscf=.f. dirscf=.t. diis=.t. CONV=1.0d-06  $end\n"
+        " $basis gbasis=n31 ngauss=6 ndfunc=1 $end\n"
+        " $DAMP IFTTYP(1)=2,0 IFTFIX(1)=1,1 thrsh=500.0 $end\n"
+        " $MAKEFP  POL=.t. DISP=.f. CHTR=.f.  EXREP=.f. $end\n"
+        " $data\n"
+        " " + filename.split('.')[0] + "\n"
+        " C1\n"
+    )
+    txt.append(header)
+    
+    # Append each atom line.
     for atom in fragment:
         if 'H000' in atom:
+            # Virtual hydrogen atoms are taken as is.
             txt.append(atom)
         else:
-            if(atom.split()[2][0]=='M'):
-                col1=' '+atom.split()[2][0:2]+atom.split()[3]+' '
-                col2=at_sym['MG']
-                filename='cla_h_'+atom.split()[0]+'.inp'
+            # Format the lines.
+            col1 = f" {atom.split()[2]}".ljust(6)
+            # Determine the atomic number using the at_sym dictionary.
+            #    WARNING: Atoms with >1 letter symbols must be added if you have them.
+            if atom.split()[2][0] == 'M':
+                col2 = at_sym['MG']
             else:
-                col1=' '+atom.split()[2][0]+atom.split()[3]+'   '
-                col2=at_sym[atom.split()[2][0]]
-            x=float(atom.split()[4])*10
-            #col3=(str(x))
-            col3=str("{:.8f}".format(x))
-            while(len(col3)<17):
-                col3=' '+col3
-            y=float(atom.split()[5])*10
-            col4=str("{:.8f}".format(y))
-            while(len(col4)<18):
-                col4=' '+col4
-            z=float(atom.split()[6])*10
-            col5=str("{:.8f}".format(z))
-            while(len(col5)<18):
-                col5=' '+col5
-            txt.append(col1+col2+col3+col4+col5+'\n')
-    txt.append(' $end \n'+
-' $comment Atoms to be erased:  $end\n')
-    with open(filename,'w') as outfile:
-        for line1 in txt:
-            outfile.write(line1)
-            
-at_sym={'H':'1.0','C':'6.0','N':'7.0','O':'8.0','MG':'12.0','P':'15.0','S':'16.0','FE':'26.0',
-        'NA':'11.0','CL':'17.0'
-      }
-
-Rings=['MG','CHA','CHB','CHC','CHD','NA','C1A','C2A','C3A','C4A','CMA','NB','C1B','C2B','C3B',
-       'C4B','CMB','CAB','CBB','NC','C1C','C2C','C3C','C4C','CMC','CAC','CBC','ND','C1D','C2D',
-       'C3D','C4D','CMD','CAD','OBD','CBD','CGD','O1D','O2D','CED','CAA','CBA','CGA','O1A',
-       'O2A','C1','C2','C3','C4','C5','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11',
-       'H12','H13','H14','H15','H16','H17','H18','H19','H20','H21','H22','H23','H24','H25',
-       'H26','H27','H28','H29','H30','H31','H32','H33','H34','H35','H36','H37','H38','H39',
-       'H40','H41']
-
-#with open('bchl359-50028.g96','r') as g96:
-#    g0=g96.readlines()
-
-with open(g96_file,'r') as g96:
-    g0=g96.readlines()
+                col2 = at_sym[atom.split()[2][0]]
+            # nm -> angstrom.
+            x, y, z = [float(atom.split()[i]) * 10 for i in range(4, 7)]
+            col3 = f"{x:.8f}".rjust(17)
+            col4 = f"{y:.8f}".rjust(18)
+            col5 = f"{z:.8f}".rjust(18)
+            txt.append(f"{col1}{col2}{col3}{col4}{col5}\n")
     
-#tailside and headside represent the atoms that are bound but are to be broken to create 
-#separate fragment files.
-curr_head=[]
-curr_tail=[]
-#BCLs=[]
-for line in g0:
+    # Append comments regarding atoms to be removed.
+    txt.append(" $end \n!comment atoms to be erased:")
+    # The QM region might include the entire chlorophyll or just the headring.
+    # In case of only headring, a tail fragment must still be created, and
+    # C6 is an MM atom covalently bound to C5, a QM atom.
+    if fragment[0].split()[0] in site:
+        txt.append(" C6")
+    txt.append("\n")
+    txt.append("!polarization points to remove:")
+    if fragment[0].split()[0] in site:
+        #H42, H43, C7 are bound to C6, polarizability must be romoved
+        txt.append(" H42 H43 C7")
+    
+    # Write the output to a file.
+    with open(filename, 'w') as outfile:
+        outfile.writelines(txt)
+
+# ---------------------------
+# Global Dictionaries and Lists
+# ---------------------------
+# Dictionary to format atomic symbols to numbers (for output).
+at_sym = {
+    'H': '1.0', 'C': '6.0', 'N': '7.0', 'O': '8.0', 'MG': '12.0',
+    'P': '15.0', 'S': '16.0', 'FE': '26.0', 'NA': '11.0', 'CL': '17.0'
+}
+
+# List of headring atoms. List includes names for YB's BCL and Reppert group's CLA.
+Rings = [
+    'MG','CHA','CHB','CHC','CHD','NA','C1A','C2A','C3A','C4A','CMA','NB',
+    'C1B','C2B','C3B','C4B','CMB','CAB','CBB','NC','C1C','C2C','C3C','C4C',
+    'CMC','CAC','CBC','ND','C1D','C2D','C3D','C4D','CMD','CAD','OBD','CBD',
+    'CGD','O1D','O2D','CED','CAA','CBA','CGA','O1A','O2A','C1','C2','C3','C4',
+    'C5','H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12','H13',
+    'H14','H15','H16','H17','H18','H19','H20','H21','H22','H23','H24','H25',
+    'H26','H27','H28','H29','H30','H31','H32','H33','H34','H35','H36','H37',
+    'H38','H39','H40','H41'
+]
+
+# ---------------------------
+# File Reading and Fragment Identification
+# ---------------------------
+# Read the EFP region file.
+with open(g96_file, 'r') as efp:
+    efp_lines = efp.readlines()
+
+# Read the full configuration file.
+with open(full_g96_file, 'r') as g96:
+    full_lines = g96.readlines()
+
+# Identify residue numbers (from lines containing 'CLA') in the EFP file that are not in the 'site' list.
+frag_cla_resnums = []
+for line in efp_lines:
     if 'CLA' in line:
-        if(oldres!=line.split()[0]):
-            head_coord,tail_coord = cut_frag(head_cut,tail_cut)
-            curr_head.append(' H000    1.0      '+str("{:.8f}".format(head_coord[0]))+'       '+str("{:.8f}".format(head_coord[1]))+'       '+str("{:.8f}".format(head_coord[2]))+'\n')
-            curr_tail.append(' H000    1.0      '+str("{:.8f}".format(tail_coord[0]))+'       '+str("{:.8f}".format(tail_coord[1]))+'       '+str("{:.8f}".format(tail_coord[2]))+'\n')
-            #if(oldres!=site):
+        res_num = line.split()[0]
+        if res_num in frag_cla_resnums:
+            continue
+        elif res_num not in site:
+            frag_cla_resnums.append(res_num)
+
+# ---------------------------
+# Fragment Processing for Head and Tail Groups
+# ---------------------------
+# 'tailside' and 'headside' are the atoms where bonds are to be broken to create
+# separate fragment files.
+curr_head = []   # Append lines for the "head" fragment
+curr_tail = []   # append lines for the "tail" fragment
+head_cut = ''    # Temporary storage for the head atom used for virtual bond creation
+
+# Process each line in the full configuration file.
+for line in full_lines:
+    if 'CLA' in line:
+        # Process only for fragments with residue numbers in our list.
+        if line.split()[0] in frag_cla_resnums and (oldres != line.split()[0]):
+            # If no head_cut has been found, update oldres and continue.
+            if len(head_cut) < 3:
+                oldres = line.split()[0]
+                continue
+            # Once both a head and tail line have been recorded, compute virtual hydrogen positions.
+            head_coord, tail_coord = cut_frag(head_cut, tail_cut)
+            # Append the virtual hydrogen lines to the corresponding fragment lists.
+            curr_head.append(
+                f" H000 1.0" +
+                f"{head_coord[0]:.8f}".rjust(17) +
+                f"{head_coord[1]:.8f}".rjust(18) +
+                f"{head_coord[2]:.8f}".rjust(18) + "\n"
+            )
+            curr_tail.append(
+                f" H000 1.0" +
+                f"{tail_coord[0]:.8f}".rjust(17) +
+                f"{tail_coord[1]:.8f}".rjust(18) +
+                f"{tail_coord[2]:.8f}".rjust(18) + "\n"
+            )
+            # Reset head_cut for the next fragment.
+            head_cut = ''
+            # Depending on whether the residue is in the 'site' list, write both fragments or only the tail.
             if oldres not in site:
                 make_inp(curr_head)
                 make_inp(curr_tail)
-            curr_head=[]
-            curr_tail=[]
-            oldres=line.split()[0]
-        atomname=line.split()[2]
-        if atomname in Rings:
-            curr_head.append(line)
-        else:
-            curr_tail.append(line)
-        if(atomname==tailside):
-            tail_cut=line
-        elif(atomname==headside):
-            head_cut=line
+            else:
+                make_inp(curr_tail)
+            # Reset the current fragment accumulators.
+            curr_head = []
+            curr_tail = []
+            oldres = line.split()[0]
         
-        
+        # Get the current atom name.
+        atomname = line.split()[2]
+        # Append the line into the desired fragment based on the Rings list.
+        if line.split()[0] in frag_cla_resnums:
+            if atomname in Rings:
+                curr_head.append(line)
+            else:
+                curr_tail.append(line)
+            # Save the line for later use if it matches the headside or tailside.
+            if atomname == tailside:
+                tail_cut = line
+            elif atomname == headside:
+                head_cut = line
